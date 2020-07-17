87a47a29717e7280430559bd1e0e8429536e20b1
daydayupyan <31303376+daydayupyan@users.noreply.github.com>
2020-07-17 10:05:32 +0800

    Update PrintTransferStockServiceImpl.java

--- src/main/java/io/juzhen/service/impl/PrintTransferStockServiceImpl.java
+++ src/main/java/io/juzhen/service/impl/PrintTransferStockServiceImpl.java
@@ -29,7 +29,7 @@ public class PrintTransferStockServiceImpl extends AssemblePDFData {
     protected Map getHeadInfo(Map map) {
         Map<String,String> headInfo = new HashMap<>();
         String className = PrintTransferStockVO.class.getSimpleName();
-        headInfo.put("firstHeadInfo","浙江股权交易中心    托管公司（管理）");
+        headInfo.put("firstHeadInfo","某某交易中心    托管公司（管理）");
         headInfo.put("secondHeadInfo","【非交易过户】凭证");
         headInfo.put("orderId",((PrintTransferStockVO)map.get(className)).getOrderId());
         return headInfo;
